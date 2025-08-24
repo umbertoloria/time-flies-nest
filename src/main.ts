bf6bb7d6fd48cac3f78eac5450a3641a92a5ab95
Umberto Loria <umberto.loria@gmail.com>
2025-08-24 20:47:00 +0200

    CORS config for localhost

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: ['Content-Type'], // Specify allowed headers
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
