1df6a06cf3665ed1e5c3e5d7fd3669a0592df50b
Umberto Loria <umberto.loria@gmail.com>
2025-08-24 20:55:27 +0200

    CORS config done

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const whitelist = [
+  'http://localhost:3000',
+  'http://umbertoloria.com:80',
+  'https://umbertoloria.com:443',
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: (origin: any, callback: any) => {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        callback(null, true);
+      } else {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: ['Content-Type'], // Specify allowed headers
     credentials: true,
@@ -13,4 +27,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
