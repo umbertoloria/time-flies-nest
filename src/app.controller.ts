4be1fb966c5e94b7e74b5454ad83ad49047f9286
Umberto Loria <umberto.loria@gmail.com>
2025-08-30 00:04:09 +0200

    Some other API routes supported

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,9 @@ import {
   Param,
   Post,
 } from '@nestjs/common';
-import { getSDK } from './remote/sdk';
+import { getSDK, getSDKPure } from './remote/sdk';
 import { ConfigService } from '@nestjs/config';
+import { getFromConfigService } from './auth';
 
 @Controller()
 export class AppController {
@@ -15,6 +16,29 @@ export class AppController {
     // private readonly appService: AppService,
     private configService: ConfigService,
   ) {}
+
+  @Post('/auth/login')
+  async authLogin(@Body() bodyParams: any): Promise<string> {
+    // Validation
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const email = bodyParams['email'];
+    // TODO: Validate date
+    if (typeof email !== 'string' || !email) {
+      throw new BadRequestException("Param 'email' required");
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const password = bodyParams['password'];
+    // TODO: Validate date
+    if (typeof password !== 'string' || !password) {
+      throw new BadRequestException("Param 'password' required");
+    }
+
+    // Forward
+    const { phpBaseUrl, phpApiKey } = getFromConfigService(this.configService);
+    return getSDKPure(phpBaseUrl, phpApiKey, '', '')
+      .authLogin(email, password)
+      .then(JSON.stringify);
+  }
 
   @Post('/auth/status')
   async authStatus(@Body() bodyParams: any): Promise<string> {
@@ -110,6 +134,55 @@ export class AppController {
       .then(JSON.stringify);
   }
 
+  @Post('/calendar-update')
+  async updateCalendar(@Body() bodyParams: any): Promise<string> {
+    // Validation
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const calendarId = bodyParams['cid'];
+    // TODO: Validate
+    if (typeof calendarId !== 'string' || !calendarId) {
+      throw new BadRequestException("Param 'cid' required");
+    }
+    const intCalendarId = parseInt(calendarId);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const name = bodyParams['name'];
+    let realName: undefined | string = undefined;
+    if (typeof name === 'string' && !!name) {
+      realName = name;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const color = bodyParams['color'];
+    let realColor: undefined | string = undefined;
+    // TODO: Validate
+    if (typeof color === 'string' && !!color) {
+      realColor = color;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const plannedColor = bodyParams['planned-color'];
+    let realPlannedColor: undefined | string = undefined;
+    // TODO: Validate
+    if (typeof plannedColor === 'string' && !!plannedColor) {
+      realPlannedColor = plannedColor;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const usesNotes = bodyParams['uses-notes'];
+    let boolUsesNotes: undefined | boolean = undefined;
+    // TODO: Validate
+    if (usesNotes === 'true' || usesNotes === 'false') {
+      boolUsesNotes = usesNotes === 'true';
+    }
+
+    // Forward
+    return getSDK(this.configService, bodyParams)
+      .updateCalendar(intCalendarId, {
+        name: realName,
+        color: realColor,
+        plannedColor: realPlannedColor,
+        usesNotes: boolUsesNotes,
+      })
+      .then(JSON.stringify);
+  }
+
   @Post('/calendars/:cid/date/:date')
   async readCalendarDate(
     @Body() bodyParams: any,
@@ -121,11 +194,188 @@ export class AppController {
     if (Number.isNaN(calendarId)) {
       throw new NotFoundException('Invalid cid ' + cid);
     }
-    // TODO: Validate date
 
     // Forward
     return getSDK(this.configService, bodyParams)
       .readCalendarDate(calendarId, date)
       .then(JSON.stringify);
+  }
+
+  @Post('/calendars/:cid/date-create/:date')
+  async createCalendarDate(
+    @Body() bodyParams: any,
+    @Param('cid') cid: string,
+    @Param('date') date: string,
+  ): Promise<string> {
+    // Validation
+    const calendarId = parseInt(cid);
+    if (Number.isNaN(calendarId)) {
+      throw new NotFoundException('Invalid cid ' + cid);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const notes = bodyParams['notes'];
+    let realNotes: undefined | string = undefined;
+    if (typeof notes === 'string' && !!notes) {
+      realNotes = notes;
+    }
+
+    // Forward
+    return getSDK(this.configService, bodyParams)
+      .createCalendarDate(calendarId, date, realNotes)
+      .then(JSON.stringify);
+  }
+
+  @Post('/calendars/:cid/date-upd-notes/:date')
+  async updateCalendarDate(
+    @Body() bodyParams: any,
+    @Param('cid') cid: string,
+    @Param('date') date: string,
+  ): Promise<string> {
+    // Validation
+    const calendarId = parseInt(cid);
+    if (Number.isNaN(calendarId)) {
+      throw new NotFoundException('Invalid cid ' + cid);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const notes = bodyParams['notes'];
+    let realNotes: undefined | string = undefined;
+    if (typeof notes === 'string' && !!notes) {
+      realNotes = notes;
+    }
+
+    // Forward
+    return getSDK(this.configService, bodyParams)
+      .updateCalendarDateNotes(calendarId, date, realNotes)
+      .then(JSON.stringify);
+  }
+
+  @Post('/calendars/:cid/todo-create/:date')
+  async createTodo(
+    @Body() bodyParams: any,
+    @Param('cid') cid: string,
+    @Param('date') date: string,
+  ): Promise<string> {
+    // Validation
+    const calendarId = parseInt(cid);
+    if (Number.isNaN(calendarId)) {
+      throw new NotFoundException('Invalid cid ' + cid);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const notes = bodyParams['notes'];
+    let realNotes: undefined | string = undefined;
+    if (typeof notes === 'string' && !!notes) {
+      realNotes = notes;
+    }
+
+    // Forward
+    return getSDK(this.configService, bodyParams)
+      .createPlannedEvent(calendarId, date, realNotes)
+      .then(JSON.stringify);
+  }
+
+  @Post('/calendars/:cid/todo-upd/:tid')
+  async updateTodoNotes(
+    @Body() bodyParams: any,
+    @Param('cid') cid: string,
+    @Param('tid') tid: string,
+  ): Promise<string> {
+    // Validation
+    const calendarId = parseInt(cid);
+    if (Number.isNaN(calendarId)) {
+      throw new NotFoundException('Invalid cid ' + cid);
+    }
+    const todoId = parseInt(tid);
+    if (Number.isNaN(calendarId)) {
+      throw new NotFoundException('Invalid tid ' + cid);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const notes = bodyParams['notes'];
+    let realNotes: undefined | string = undefined;
+    if (typeof notes === 'string' && !!notes) {
+      realNotes = notes;
+    }
+
+    // Forward
+    return getSDK(this.configService, bodyParams)
+      .updatePlannedEvent(calendarId, todoId, realNotes)
+      .then(JSON.stringify);
+  }
+
+  @Post('/calendars/:cid/todo-move/:tid')
+  async moveTodo(
+    @Body() bodyParams: any,
+    @Param('cid') cid: string,
+    @Param('tid') tid: string,
+  ): Promise<string> {
+    // Validation
+    const calendarId = parseInt(cid);
+    if (Number.isNaN(calendarId)) {
+      throw new NotFoundException('Invalid cid ' + cid);
+    }
+    const todoId = parseInt(tid);
+    if (Number.isNaN(calendarId)) {
+      throw new NotFoundException('Invalid tid ' + cid);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const date = bodyParams['date'];
+    // TODO: Validate date
+    if (typeof date !== 'string' || !date) {
+      throw new BadRequestException("Param 'date' required");
+    }
+
+    // Forward
+    return getSDK(this.configService, bodyParams)
+      .movePlannedEvent(calendarId, todoId, date)
+      .then(JSON.stringify);
+  }
+
+  @Post('/calendars/:cid/todo-done/:tid')
+  async doneOrMissedTodo(
+    @Body() bodyParams: any,
+    @Param('cid') cid: string,
+    @Param('tid') tid: string,
+  ): Promise<string> {
+    // Validation
+    const calendarId = parseInt(cid);
+    if (Number.isNaN(calendarId)) {
+      throw new NotFoundException('Invalid cid ' + cid);
+    }
+    const todoId = parseInt(tid);
+    if (Number.isNaN(calendarId)) {
+      throw new NotFoundException('Invalid tid ' + cid);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    const mode = bodyParams['mode'];
+    // TODO: Validate date
+    if (mode !== 'done' && mode !== 'missed') {
+      throw new BadRequestException("Param 'mode' invalid");
+    }
+    if (mode === 'done') {
+      // Validation
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+      const notes = bodyParams['notes'];
+      let realNotes: undefined | string = undefined;
+      if (typeof notes === 'string' && !!notes) {
+        realNotes = notes;
+      }
+
+      // Forward
+      return getSDK(this.configService, bodyParams)
+        .setPlannedEventAsDone(calendarId, todoId, {
+          type: 'done',
+          notes: realNotes,
+        })
+        .then(JSON.stringify);
+    } else if (mode === 'missed') {
+      // Forward
+      return getSDK(this.configService, bodyParams)
+        .setPlannedEventAsDone(calendarId, todoId, {
+          type: 'missed',
+        })
+        .then(JSON.stringify);
+    } else {
+      // Should never happen.
+      throw new BadRequestException("Param 'mode' invalid");
+    }
   }
 }
--- src/remote/sdk.ts
+++ src/remote/sdk.ts
@@ -19,6 +19,14 @@ export const getSDKPure = (
 
   // TODO: FormData required here
   return {
+    authLogin(email: string, password: string) {
+      const formData = new FormData();
+      formData.append('email', email);
+      formData.append('password', password);
+      return api
+        .post('?a=login', formData)
+        .then((response) => response.data as object);
+    },
     readAuthStatus() {
       const formData = new FormData();
       formData.append('em', em);
@@ -63,6 +71,35 @@ export const getSDKPure = (
         .post(`?a=calendar-create`, formData)
         .then((response) => response.data as object);
     },
+    updateCalendar(
+      calendarId: number,
+      data: {
+        name?: string;
+        color?: string; // Es. "#115599"
+        plannedColor?: string; // Es. "#115599"
+        usesNotes?: boolean;
+      },
+    ) {
+      const formData = new FormData();
+      formData.append('em', em);
+      formData.append('sp', sp);
+      formData.append('cid', `${calendarId}`);
+      if (data.name) {
+        formData.append('name', data.name);
+      }
+      if (data.color) {
+        formData.append('color', data.color);
+      }
+      if (data.plannedColor) {
+        formData.append('planned-color', data.plannedColor);
+      }
+      if (typeof data.usesNotes === 'boolean') {
+        formData.append('uses-notes', data.usesNotes ? 'true' : 'false');
+      }
+      return api
+        .post('?a=calendar-update', formData)
+        .then((response) => response.data as object);
+    },
     readCalendarByID(calendarId: number) {
       const formData = new FormData();
       formData.append('em', em);
@@ -77,6 +114,113 @@ export const getSDKPure = (
       formData.append('sp', sp);
       return api
         .post(`?a=calendar-date-read&cid=${calendarId}&date=${date}`, formData)
+        .then((response) => response.data as object);
+    },
+    createCalendarDate(
+      calendarId: number,
+      date: string,
+      notes: undefined | string,
+    ) {
+      const formData = new FormData();
+      formData.append('em', em);
+      formData.append('sp', sp);
+      formData.append('id', `${calendarId}`);
+      formData.append('local-date', date);
+      if (notes) {
+        formData.append('notes', notes);
+      }
+      return api
+        .post('?a=calendar-date-create', formData)
+        .then((response) => response.data as object);
+    },
+    updateCalendarDateNotes(
+      calendarId: number,
+      date: string,
+      notes: undefined | string,
+    ) {
+      const formData = new FormData();
+      formData.append('em', em);
+      formData.append('sp', sp);
+      formData.append('calendar-id', `${calendarId}`);
+      formData.append('local-date', date);
+      if (notes) {
+        formData.append('notes', notes);
+      }
+      return api
+        .post('?a=calendar-date-update-notes', formData)
+        .then((response) => response.data as object);
+    },
+    createPlannedEvent(
+      calendarId: number,
+      date: string,
+      notes: undefined | string,
+    ) {
+      const formData = new FormData();
+      formData.append('em', em);
+      formData.append('sp', sp);
+      formData.append('id', `${calendarId}`);
+      formData.append('local-date', date);
+      if (notes) {
+        formData.append('notes', notes);
+      }
+      return api
+        .post('?a=planned-event-create', formData)
+        .then((response) => response.data as object);
+    },
+    updatePlannedEvent(
+      calendarId: number,
+      todoId: number,
+      notes: undefined | string,
+    ) {
+      const formData = new FormData();
+      formData.append('em', em);
+      formData.append('sp', sp);
+      formData.append('cid', `${calendarId}`);
+      formData.append('eid', `${todoId}`);
+      if (notes) {
+        formData.append('notes', notes);
+      }
+      return api
+        .post('?a=planned-event-update-notes', formData)
+        .then((response) => response.data as object);
+    },
+    movePlannedEvent(calendarId: number, todoId: number, date: string) {
+      const formData = new FormData();
+      formData.append('em', em);
+      formData.append('sp', sp);
+      formData.append('cid', `${calendarId}`);
+      formData.append('eid', `${todoId}`);
+      formData.append('date', date);
+      return api
+        .post('?a=planned-event-move', formData)
+        .then((response) => response.data as object);
+    },
+    setPlannedEventAsDone(
+      calendarId: number,
+      todoId: number,
+      mode:
+        | {
+            type: 'done';
+            notes: undefined | string;
+          }
+        | {
+            type: 'missed';
+          },
+    ) {
+      const formData = new FormData();
+      formData.append('em', em);
+      formData.append('sp', sp);
+      formData.append('calendar_id', `${calendarId}`);
+      formData.append('event_id', `${todoId}`);
+      if (mode.type === 'done') {
+        if (typeof mode.notes === 'string') {
+          formData.append('notes', mode.notes);
+        }
+      } else if (mode.type === 'missed') {
+        formData.append('set_as_missed', 'true');
+      }
+      return api
+        .post('?a=planned-event-set-as-done', formData)
         .then((response) => response.data as object);
     },
   };
