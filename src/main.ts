915818b8b1362b40218ed8c47d718897a98143bf
Umberto Loria <umberto.loria@gmail.com>
2026-06-05 15:32:17 +0200

    feat: Env. variables updated

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
-const APP_PORT = parseInt('' + process.env.APP_PORT || '8663', 10);
-const APP_ADDRESS = process.env.APP_ADDRESS || '0.0.0.0';
+const PORT = parseInt('' + process.env.PORT, 10);
+// const ADDRESS = process.env.ADDRESS;
 
-const ORIGINS_WHITELIST = [
-  'http://localhost:3000',
-  // 'http://umbertoloria.com',
-  'https://umbertoloria.com',
-  'https://www.umbertoloria.com',
-  'https://tfapi.umbertoloria.com',
-] as const;
+const originWhitelist = (process.env.CORS_ORIGINS_WHITELIST || '').split(',');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -23,8 +17,10 @@ async function bootstrap() {
 
   app.enableCors({
     origin: (origin: any, callback: any) => {
-      // console.log('request from origin', origin);
-      if (!origin || ORIGINS_WHITELIST.indexOf(origin) !== -1) {
+      console.debug('originWhitelist');
+      console.debug(originWhitelist);
+      console.debug('request from origin', origin);
+      if (!origin || originWhitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
@@ -44,7 +40,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(APP_PORT, APP_ADDRESS);
+  await app.listen(PORT);
 
   const logger = new Logger('Bootstrap');
   const server = app.getHttpServer();
