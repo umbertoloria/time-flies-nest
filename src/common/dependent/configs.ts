a3564bdae7a495e20f2c9228a3fce332986b4b84
Umberto Loria <umberto.loria@gmail.com>
2026-06-09 00:47:13 +0200

    feat: unused dependencies removed

--- src/common/dependent/configs.ts
+++ src/common/dependent/configs.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 
 export const getConfigs = () => {
   return {
-    port: parseInt('' + process.env.PORT, 10),
     corsAllowedOrigins: process.env.CORS_ORIGINS_WHITELIST?.split(',') || [],
     jwtJwksUri: process.env.JWT_JWKS_URI!,
     jwtIssuer: process.env.JWT_ISSUER!,
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { createHonoServer } from '@shared/dependent/hono/server-hono';
 import { todoApp } from '@app/todo/dependent/todo.hono';
 import { calendarApp } from '@app/calendar/dependent/calendar.hono';
 import { taskApp } from '@app/task/dependent/task.hono';
-import { serve } from '@hono/node-server';
-import { getConfigs } from '@shared/dependent/configs';
 import { mapAppError2StatusCode } from '@shared/core/errors';
 import {
   getHttpErrorName,
@@ -68,13 +66,4 @@ app.onError((err, c) => {
   );
 });
 
-serve(
-  {
-    fetch: app.fetch,
-    port: getConfigs().port,
-  },
-  (info) => {
-    const { address, port } = info;
-    console.log(`Application is running on: http://${address}:${port}`);
-  },
-);
+export default app;
