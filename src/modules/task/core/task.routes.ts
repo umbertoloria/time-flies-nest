2d916f48a24c60638ea19020678df6f72eed2d91
Umberto Loria <umberto.loria@gmail.com>
2026-06-08 11:41:07 +0200

    ref: Task routes extracted from controller

---
+++ src/modules/task/core/task.routes.ts
@@ -0,0 +1,68 @@
+import { CreateTaskDto, ReadCalendarDateDto, UpdateTaskDto } from './dto';
+import { todoService } from '../../todo/todo.service';
+import { taskService } from './task.service';
+import { TCalendarSDK, TNewDoneTask } from '../../../sdk/types';
+import { calendarService } from '../../calendar/core/calendar.service';
+
+class TaskRoutes {
+  async create(
+    paramCalendarId: string,
+    body: any,
+    user: ReqUser,
+  ): Promise<TNewDoneTask> {
+    // FIXME: Ops... forgot to check if user owns the calendar...
+    const dto = CreateTaskDto.fromBody(paramCalendarId, body);
+
+    const createdTask = await taskService.createDoneTask(dto);
+
+    return createdTask.toTNewDoneTask();
+  }
+
+  async read(
+    paramCalendarId: string,
+    date: string,
+    user: ReqUser,
+  ): Promise<TCalendarSDK.ReadDateResponse> {
+    // TODO: Responds with both Tasks and Todos...
+    const dto = ReadCalendarDateDto.fromBody(paramCalendarId, date, user);
+
+    // BL
+    const calendar = await calendarService.findCalendarFromUser(
+      dto.calendarId,
+      dto.user.id,
+    );
+
+    const undoneTodos = await todoService.findUndoneTodosByCalendar(
+      calendar.id,
+      dto.date,
+    );
+
+    const doneTasks = await taskService.findTasksFromCalendarAndDate(
+      dto.calendarId,
+      dto.date,
+    );
+
+    return {
+      calendar: calendar.toTCalendarRcd(),
+      date: dto.date,
+      doneTasks: doneTasks.map((doneTask) => doneTask.toTNewDoneTask()),
+      todos: undoneTodos.map((todo) => todo.toTNewTodo()),
+    };
+  }
+
+  async update(
+    paramCalendarId: string,
+    paramTaskId: string,
+    body: any,
+    user: ReqUser,
+  ) {
+    // FIXME: Ops... forgot to check if user owns the calendar...
+    const dto = UpdateTaskDto.fromBody(paramCalendarId, paramTaskId, body);
+
+    const updatedTask = await taskService.updateTaskNotesByDate(dto);
+
+    return updatedTask.toTNewDoneTask();
+  }
+}
+
+export const taskRoutes = new TaskRoutes();
--- src/modules/task/dependent/task.controller.ts
+++ src/modules/task/dependent/task.controller.ts
@@ -1,73 +1,39 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import { taskService } from '../core/task.service';
 import { TCalendarSDK, TNewDoneTask } from '../../../sdk/types';
-import { calendarService } from '../../calendar/core/calendar.service';
-import { todoService } from '../../todo/todo.service';
 import {
   AccessTokenGuard,
   CurrentUser,
 } from '../../../lib/guards/access-token.guard';
-import { CreateTaskDto, ReadCalendarDateDto, UpdateTaskDto } from '../core/dto';
+import { taskRoutes } from '../core/task.routes';
 
 @UseGuards(AccessTokenGuard)
 @Controller('/calendars/:cid/date')
 export class TaskController {
   @Post()
-  async create(
-    @Body() body: any,
+  create(
     @Param('cid') paramCalendarId: string,
+    @Body() body: any,
+    @CurrentUser() user: ReqUser,
   ): Promise<TNewDoneTask> {
-    const dto = CreateTaskDto.fromBody(paramCalendarId, body);
-
-    const createdTask = await taskService.createDoneTask(dto);
-
-    return createdTask.toTNewDoneTask();
+    return taskRoutes.create(paramCalendarId, body, user);
   }
 
   @Get(':date')
-  async read(
-    // @Body() body: any,
+  read(
     @Param('cid') paramCalendarId: string,
     @Param('date') date: string,
     @CurrentUser() user: ReqUser,
   ): Promise<TCalendarSDK.ReadDateResponse> {
-    // TODO: Responds with both Tasks and Todos...
-    const dto = ReadCalendarDateDto.fromBody(paramCalendarId, date, user);
-
-    // BL
-    const calendar = await calendarService.findCalendarFromUser(
-      dto.calendarId,
-      dto.user.id,
-    );
-
-    const undoneTodos = await todoService.findUndoneTodosByCalendar(
-      calendar.id,
-      dto.date,
-    );
-
-    const doneTasks = await taskService.findTasksFromCalendarAndDate(
-      dto.calendarId,
-      dto.date,
-    );
-
-    return {
-      calendar: calendar.toTCalendarRcd(),
-      date: dto.date,
-      doneTasks: doneTasks.map((doneTask) => doneTask.toTNewDoneTask()),
-      todos: undoneTodos.map((todo) => todo.toTNewTodo()),
-    };
+    return taskRoutes.read(paramCalendarId, date, user);
   }
 
   @Post(':tid')
   async update(
-    @Body() body: any,
     @Param('cid') paramCalendarId: string,
     @Param('tid') paramTaskId: string,
+    @Body() body: any,
+    @CurrentUser() user: ReqUser,
   ): Promise<TNewDoneTask> {
-    const dto = UpdateTaskDto.fromBody(paramCalendarId, paramTaskId, body);
-
-    const updatedTask = await taskService.updateTaskNotesByDate(dto);
-
-    return updatedTask.toTNewDoneTask();
+    return taskRoutes.update(paramCalendarId, paramTaskId, body, user);
   }
 }
