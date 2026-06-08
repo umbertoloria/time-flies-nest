3a00ad22398c3cc1bac54f7eba02efd43a4ec138
Umberto Loria <umberto.loria@gmail.com>
2026-06-09 00:22:53 +0200

    feat: using Bun and Prisma 7

--- src/common/dependent/prisma.repository.ts
+++ src/common/dependent/prisma.repository.ts
@@ -1,12 +1,18 @@
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import {
   Calendar as PrismaCalendar,
   PrismaClient,
   Task as PrismaTask,
   Todo as PrismaTodo,
-} from '@prisma/client';
+} from '../../generated/prisma/client';
 
 export type Calendar = PrismaCalendar;
 export type Task = PrismaTask;
 export type Todo = PrismaTodo;
 
-export const prisma = new PrismaClient();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter });
