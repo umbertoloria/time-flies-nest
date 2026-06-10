395869f463001430c7ce85d9fcf3a86a19df6011
Umberto Loria <umberto.loria@gmail.com>
2026-06-10 22:08:07 +0200

    debug: test Env. config

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,8 +1,15 @@
 export const getConfig = () => {
-  return {
+  const result = {
     corsAllowedOrigins: process.env.CORS_ORIGINS_WHITELIST?.split(',') || [],
     jwtJwksUri: process.env.JWT_JWKS_URI!,
     jwtIssuer: process.env.JWT_ISSUER!,
     jwtAudience: process.env.JWT_AUDIENCE!,
   };
+  console.debug(result.corsAllowedOrigins);
+  console.debug(result.jwtJwksUri);
+  console.debug(result.jwtIssuer);
+  console.debug(result.jwtAudience);
+  console.debug(result);
+  console.debug(JSON.stringify(result, null, 2));
+  return result;
 };
