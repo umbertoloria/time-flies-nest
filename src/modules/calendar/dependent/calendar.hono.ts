10f4cbdefa293262d5d03a60d35855dfe37e6ce0
Umberto Loria <umberto.loria@gmail.com>
2026-06-10 17:33:34 +0200

    fix: *Routes typing improved in Hono Endpoints

--- src/modules/calendar/dependent/calendar.hono.ts
+++ src/modules/calendar/dependent/calendar.hono.ts
@@ -1,50 +1,51 @@
 import { Hono } from 'hono';
 import { HonoEnv } from '@dep/hono';
 import { ReadCalendarsGdtoSchema, UpdateCalendarGdtoSchema } from './gdto';
+import { CalendarRoutes } from '@app/calendar/core/calendar.routes';
 
 const app = new Hono<HonoEnv>();
 
 app.get('/calendars', async (c) => {
-  const ctx = c.get('ctx');
+  const calendarRoutes: CalendarRoutes = c.get('ctx').calendarRoutes;
   const user = c.get('user');
   const query = c.req.query();
 
   const gdto = ReadCalendarsGdtoSchema.parse(query);
 
-  const response = await ctx.calendarRoutes.readAll(gdto, user);
+  const response = await calendarRoutes.readAll(gdto, user);
 
   return c.json(response);
 });
 
 app.post('/calendars', async (c) => {
-  const ctx = c.get('ctx');
+  const calendarRoutes: CalendarRoutes = c.get('ctx').calendarRoutes;
   const user = c.get('user');
   const body = await c.req.json();
 
-  const response = await ctx.calendarRoutes.create(body, user);
+  const response = await calendarRoutes.create(body, user);
 
   return c.json(response);
 });
 
 app.get('/calendars/:id', async (c) => {
-  const ctx = c.get('ctx');
+  const calendarRoutes: CalendarRoutes = c.get('ctx').calendarRoutes;
   const user = c.get('user');
   const paramCalendarId = c.req.param('id');
 
-  const response = await ctx.calendarRoutes.read(paramCalendarId, user);
+  const response = await calendarRoutes.read(paramCalendarId, user);
 
   return c.json(response);
 });
 
 app.post('/calendars/:id', async (c) => {
-  const ctx = c.get('ctx');
+  const calendarRoutes: CalendarRoutes = c.get('ctx').calendarRoutes;
   const user = c.get('user');
   const paramCalendarId = c.req.param('id');
   const body = await c.req.json();
 
   const gdto = UpdateCalendarGdtoSchema.parse(body);
 
-  const response = await ctx.calendarRoutes.update(paramCalendarId, gdto, user);
+  const response = await calendarRoutes.update(paramCalendarId, gdto, user);
 
   return c.json(response);
 });
--- src/modules/todo/dependent/todo.hono.ts
+++ src/modules/todo/dependent/todo.hono.ts
@@ -1,36 +1,37 @@
 import { Hono } from 'hono';
 import { HonoEnv } from '@dep/hono';
+import { TodoRoutes } from '@app/todo/core/todo.routes';
 
 const app = new Hono<HonoEnv>();
 
 app.get('/calendars/streamline', async (c) => {
-  const ctx = c.get('ctx');
+  const todoRoutes: TodoRoutes = c.get('ctx').todoRoutes;
   const user = c.get('user');
 
-  const response = await ctx.todoRoutes.readStreamline(user);
+  const response = await todoRoutes.readStreamline(user);
 
   return c.json(response);
 });
 
 app.post('/calendars/:id/todo', async (c) => {
-  const ctx = c.get('ctx');
+  const todoRoutes: TodoRoutes = c.get('ctx').todoRoutes;
   const user = c.get('user');
   const paramCalendarId = c.req.param('id');
   const body = await c.req.json();
 
-  const response = await ctx.todoRoutes.create(paramCalendarId, body, user);
+  const response = await todoRoutes.create(paramCalendarId, body, user);
 
   return c.json(response);
 });
 
 app.post('/calendars/:id/todo/:tid/update-notes', async (c) => {
-  const ctx = c.get('ctx');
+  const todoRoutes: TodoRoutes = c.get('ctx').todoRoutes;
   const user = c.get('user');
   const paramCalendarId = c.req.param('id');
   const paramTodoId = c.req.param('tid');
   const body = await c.req.json();
 
-  const response = await ctx.todoRoutes.updateTodoNotes(
+  const response = await todoRoutes.updateTodoNotes(
     paramCalendarId,
     paramTodoId,
     body,
@@ -41,13 +42,13 @@ app.post('/calendars/:id/todo/:tid/update-notes', async (c) => {
 });
 
 app.post('/calendars/:id/todo/:tid/move', async (c) => {
-  const ctx = c.get('ctx');
+  const todoRoutes: TodoRoutes = c.get('ctx').todoRoutes;
   const user = c.get('user');
   const paramCalendarId = c.req.param('id');
   const paramTodoId = c.req.param('tid');
   const body = await c.req.json();
 
-  const response = await ctx.todoRoutes.moveTodo(
+  const response = await todoRoutes.moveTodo(
     paramCalendarId,
     paramTodoId,
     body,
@@ -58,13 +59,13 @@ app.post('/calendars/:id/todo/:tid/move', async (c) => {
 });
 
 app.post('/calendars/:id/todo/:tid/set-as-done', async (c) => {
-  const ctx = c.get('ctx');
+  const todoRoutes: TodoRoutes = c.get('ctx').todoRoutes;
   const user = c.get('user');
   const paramCalendarId = c.req.param('id');
   const paramTodoId = c.req.param('tid');
   const body = await c.req.json();
 
-  const response = await ctx.todoRoutes.updateTodoSetAsDone(
+  const response = await todoRoutes.updateTodoSetAsDone(
     paramCalendarId,
     paramTodoId,
     body,
